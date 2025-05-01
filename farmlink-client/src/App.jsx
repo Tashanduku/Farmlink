5ad1a3dd33589d2a0ad68ac0170f7a52ee228ad1
Cheriroh <prudencec2019@gmail.com>
2025-05-01 16:31:29 +0300

    changes made

--- farmlink-client/src/App.jsx
+++ farmlink-client/src/App.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import BlogDetail from './pages/BlogDetail';
+import ExpertList from './components/experts/ExpertList';
+import CommunityList from './components/communities/CommunityList'; // ✅ Add this import
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/blogs/:id" element={<BlogDetail />} />
-        {/* Add more routes as needed */}
+        <Route path="/blogs" element={<BlogDetail />} />
+        <Route path="/communities" element={<CommunityList />} />
+        <Route path='/experts' element={<ExpertList/>} />
       </Routes>
     </Router>
   );

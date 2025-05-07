10be3c4a3ae6ba6dbbf23ca4735ce4b22830b015
SilviaApadet <sylviaapadet01@gmail.com>
2025-05-07 10:23:54 +0300

    updated

--- farmlink-client/src/App.jsx
+++ farmlink-client/src/App.jsx
@@ -1,21 +1,62 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import BlogDetail from './pages/BlogDetail';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';  // Single import statement
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+import { AuthProvider } from './contexts/AuthContext';
+
+import Layout from './components/common/Layout';
+import Welcome from './pages/Welcome';
+import CreateBlog from './pages/CreateBlog';
 import ExpertList from './components/experts/ExpertList';
-import CommunityList from './components/communities/CommunityList'; // ✅ Add this import
+import CommunityList from './components/communities/CommunityList';
+import Profile from './pages/Profile';
+import BlogList from './components/blogs/BlogList';  // Updated to import BlogList
+import Home from './pages/Home';  // Assuming you have a Home page
+import BlogDetail from './pages/BlogDetail';  // Assuming BlogDetail exists
+import Login from './pages/Login';  // Assuming Login exists
+import SignUp from './pages/Signup';  // Assuming SignUp exists
+
+// Private route component
+const PrivateRoute = ({ element }) => {
+  const isAuthenticated = localStorage.getItem('token') !== null;
+  return isAuthenticated ? element : <Navigate to="/login" />;
+};
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/blogs" element={<BlogDetail />} />
-        <Route path="/communities" element={<CommunityList />} />
-        <Route path='/experts' element={<ExpertList/>} />
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <AuthProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              {/* Public routes */}
+              <Route index element={<Welcome />} />
+              <Route path="login" element={<Login />} />
+              <Route path="signup" element={<SignUp />} />
+
+              {/* Main app routes */}
+              <Route path="home" element={<Home />} />
+              <Route path="blogs" element={<BlogList />} />
+              <Route path="blogs/create" element={<CreateBlog />} />
+              <Route path="blogs/:id" element={<BlogDetail />} />
+              <Route path="communities" element={<CommunityList />} />
+              <Route path="experts" element={<ExpertList />} />
+              <Route path="profile" element={<Profile />} />
+            </Route>
+
+            {/* Protected routes */}
+            <Route path="home" element={<PrivateRoute element={<Home />} />} />
+            <Route path="blogs" element={<PrivateRoute element={<BlogList />} />} />
+            <Route path="blogs/create" element={<PrivateRoute element={<CreateBlog />} />} />
+            <Route path="blogs/:id" element={<BlogDetail />} />
+            <Route path="communities" element={<PrivateRoute element={<CommunityList />} />} />
+            <Route path="profile" element={<PrivateRoute element={<Profile />} />} />
+          </Routes>
+        </Router>
+      </AuthProvider>
+    </Provider>
   );
 }
 
 export default App;
+
